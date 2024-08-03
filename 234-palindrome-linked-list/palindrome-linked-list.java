d94852e68e6b5e9084dba68f02d67bb89ccb5615
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2024-08-03 11:22:28 +0530

    Time: 12 ms (21.13%) | Memory: 75.1 MB (5.39%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,18 +9,16 @@
  * }
  */
 class Solution {
+    ListNode curr;
     public boolean isPalindrome(ListNode head) {
-       List<Integer> list=new ArrayList<Integer>();
-        while(head!=null){
-           list.add(head.val);
-           head=head.next;
-        }
-        int n=list.size();
-        for(int i=0;i<list.size()/2;i++){
-            if(list.get(i)!=list.get(n-1-i)){
-               return false;
-            }
-        }
-        return true;
+        curr = head;
+        return solve(head);
+    }
+
+    public boolean solve(ListNode head) {
+        if(head == null) return true;
+        boolean ans = solve(head.next) && head.val == curr.val;
+        curr = curr.next;
+        return ans;
     }
 }
