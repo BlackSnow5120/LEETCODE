093d1030d314086fd74bb95ed90a5e04c4209d10
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2024-08-22 10:04:41 +0530

    Time: 35 ms (12.17%) | Memory: 38.5 MB (53.42%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& vec) {
-        int mul=1;
-        int flag=0;
-        int count=0;
-        for(auto i : vec)
-        {
-            if(i!=0) mul*=i;
-            else count++;
-        }
-        for(int i=0;i<vec.size();i++)
-        {
-            if(count > 1)
-            {
-                vec[i]=0;
-            }
+        int mul = 1;
+        int flag = 0;
+        int count = 0;
+        for (auto i : vec) {
+            if (i != 0)
+                mul *= i;
             else
-            {
-                if(count==1)
-                {
-                if(vec[i]==0)
-                {
-                    vec[i]=mul;
-                }
-                else
-                {
-                    vec[i]=0;
-                }
-                }
-                else
-                {
-                    vec[i]=mul/vec[i];
+                count++;
+        }
+        for (int i = 0; i < vec.size(); i++) {
+            if (count > 1) {
+                vec[i] = 0;
+            } else {
+                if (count == 1) {
+                    if (vec[i] == 0) {
+                        vec[i] = mul;
+                    } else {
+                        vec[i] = 0;
+                    }
+                } else {
+                    vec[i] = mul / vec[i];
                 }
             }
         }
-        cout<<count;
         return vec;
     }
 };
