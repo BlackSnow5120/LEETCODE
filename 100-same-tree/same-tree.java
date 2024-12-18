3fa5477d2754a4213027ebd42208b0ac316607a6
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2024-12-18 09:49:56 +0530

    Time: 2 ms (100.00%) | Memory: 41 MB (51.74%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        ArrayList <Integer> arr1 = new ArrayList<>();
+        ArrayList <Integer> arr2 = new ArrayList<>();
+
+        Queue <TreeNode> q1 = new LinkedList<>();
+        Queue <TreeNode> q2 = new LinkedList<>();
+
+        q1.add(p);
+        q2.add(q);
+        while(!q1.isEmpty())
+        {
+            TreeNode temp = q1.poll();
+            if(temp!=null)
+            {
+                arr1.add(temp.val);
+                q1.add(temp.left);
+                q1.add(temp.right);
+            }
+            else
+            {
+                arr1.add(null);
+            }
+        }
+                while(!q2.isEmpty())
+        {
+            TreeNode temp = q2.poll();
+            if(temp!=null)
+            {
+                arr2.add(temp.val);
+                q2.add(temp.left);
+                q2.add(temp.right);
+            }
+            else
+            {
+                arr2.add(null);
+            }
+        }
+        System.out.print(arr1);
+        return Arrays.equals(arr1.toArray(),arr2.toArray());
+    }
+}
