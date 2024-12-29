9c4ad9901e1bfc055fe6ba4f81d2fb72bf43e7db
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2024-12-29 13:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (10.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
         int len = nums.size();
-        int zero = 0;
-        while (i < len) {
-            if (nums[i] == 0) {
-                nums.erase(nums.begin() + i);
-                len--; 
-                zero++;
-            } else {
-                i++; 
+        int j = 0; 
+        for (int i = 0; i < len; ++i) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
             }
         }
-        for (int j = 0;j<zero;j++) {
-            nums.push_back(0);
+
+        while (j < len) {
+            nums[j++] = 0;
         }
     }
 };
