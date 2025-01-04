b1d88b542b1aefe83d2d92ce397c19d1f8cf40fc
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-01-05 03:06:12 +0530

    Time: 3 ms (30.68%) | Memory: 16.3 MB (38.52%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int func(int i,int j ,vector<vector<int>> &dp,int m,int n,vector<vector<int>>& ob)
+    {
+        if(i==m || j==n) return INT_MAX;
+        if(i==m-1 && j==n-1) return ob[i][j];
+        if(dp[i][j]!= -1) return dp[i][j];
+        int down = func(i+1,j,dp,m,n,ob);
+        int right = func(i,j+1,dp,m,n,ob);
+        dp[i][j] = ob[i][j] + min(down,right);
+        return dp[i][j];
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n= grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return func(0,0,dp,m,n,grid);
+    }
+};
