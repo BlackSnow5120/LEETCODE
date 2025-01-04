9925beb267c241cdeee8e32e631f0356ce7351f5
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-01-05 03:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (10.14%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int func(int i,int j ,vector<vector<int>> &dp,int m,int n,vector<vector<int>>& ob)
     {
-        if(i==m || j==n) return INT_MAX;
+        if(i==m || j==n) return 9999999;
         if(i==m-1 && j==n-1) return ob[i][j];
         if(dp[i][j]!= -1) return dp[i][j];
         int down = func(i+1,j,dp,m,n,ob);
