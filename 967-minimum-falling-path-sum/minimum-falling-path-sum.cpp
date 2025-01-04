d4947faf6ec33bc642b335835047108a30a026b5
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-01-05 03:32:11 +0530

    Time: 3 ms (31.09%) | Memory: 15 MB (43.11%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int>>& dp, int m, vector<vector<int>>& grid) {
-        if (j < 0 || j >= m) return INT_MAX; // Out of bounds for columns
-        if (i == m - 1) return grid[i][j];  // Base case: bottom row
-        if (dp[i][j] != INT_MIN) return dp[i][j]; // Return memoized result
-
-        int down = func(i + 1, j, dp,m, grid);
-        int left = func(i + 1, j - 1, dp, m,grid);
-        int right = func(i + 1, j + 1, dp, m,grid);
-
-        return dp[i][j]= grid[i][j] + min(down,min(right,left));
-    }
-int minFallingPathSum(vector<vector<int>>& matrix) {
+    int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         vector<vector<int>> dp(n,vector<int>(n,INT_MIN));
         int ans=INT_MAX;
         for(int i=0;i<n;i++){
-            ans=min(ans,func(0,i,dp,n,matrix));
+            ans=min(ans,f(matrix,0,i,dp));
         }
         return ans;
+    }
+private:
+    int f(vector<vector<int>>& matrix,int i,int j,vector<vector<int>>& dp){
+        if(j<0) return INT_MAX;
+        if(j>=matrix.size()) return INT_MAX;
+        if(i==matrix.size()-1) return matrix[i][j];
+        if(dp[i][j]!=INT_MIN) return dp[i][j];
+        int a=f(matrix,i+1,j-1,dp);
+        int b=f(matrix,i+1,j,dp);
+        int c=f(matrix,i+1,j+1,dp);
+        return dp[i][j]=min(a,min(b,c))+matrix[i][j];
     }
 };
