b245542aac8e7aa6ca9c284c274df2e2bc085e8b
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-03-09 19:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.39%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -12,7 +12,7 @@ public:
 
             if (nums[low] <= nums[mid]) {
                 if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
+                    high = mid ;
                 } else {
                     low = mid + 1;
                 }
@@ -20,7 +20,7 @@ public:
                 if (nums[mid] < target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
-                    high = mid - 1;
+                    high = mid;
                 }
             }
         }
