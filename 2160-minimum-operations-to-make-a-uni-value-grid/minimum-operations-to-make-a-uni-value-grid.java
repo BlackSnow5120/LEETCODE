7ec9907c22b67ebcc9fab8c652dbbc7dd59ae6c7
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-03-26 11:33:32 +0530

    Time: 38 ms (66.83%) | Memory: 78.2 MB (6.03%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.java
@@ -1,29 +1,30 @@
+
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int n = grid.length;
-        int m = grid[0].length;
-        List<Integer> l1 = new ArrayList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                l1.add(grid[i][j]);
-            }
-        }
-        int mid = (l1.size()) / 2;
-        int flag = 0;
-        l1.sort((a, b) -> Integer.compare(a, b));
-        int num = l1.get(mid);
-        int ans = 0;
-        for (int i : l1) {
-            if (flag == 0) {
-                if (Math.abs(i - num) % x == 0) {
-                    ans += Math.abs(i - num) / x;
-                } else {
-                    flag = 1;
-                }
-            }
-        }
-        if(flag==0) return ans;
-        return -1;
+        int n = grid.length, m = grid[0].length;
+        int size = n * m;
+        int[] arr = new int[size];
 
+        // Flatten the grid into an array
+        int index = 0;
+        for (int[] row : grid) {
+            for (int num : row) {
+                arr[index++] = num;
+            }
+        }
+
+        // Sort the array to find the median
+        Arrays.sort(arr);
+        int median = arr[size / 2];
+
+        // Calculate operations
+        int operations = 0;
+        for (int num : arr) {
+            int diff = Math.abs(num - median);
+            if (diff % x != 0) return -1;  // If transformation is impossible
+            operations += diff / x;
+        }
+
+        return operations;
     }
 }
