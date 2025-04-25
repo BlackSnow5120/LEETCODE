5d0c0c3f01867ce026ed0dea4dbf9aa77f151fd6
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-04-26 02:20:46 +0530

    Time: 3 ms (58.83%) | Memory: 43.2 MB (24.32%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,32 +1,29 @@
 class Solution {
-    public String func1(String s)
-    {
+    public String func1(String s) {
         int len = s.length();
         int i = 0;
-        String ans="";
+        StringBuilder ans = new StringBuilder();
         Integer count = 0;
         char temp = s.charAt(0);
-        while(i<len)
-        {
-            if(s.charAt(i)!=temp)
-            {
-                ans+=count.toString();
-                ans+=temp;
-                temp=s.charAt(i);
-                count=0;
+        while (i < len) {
+            if (s.charAt(i) != temp) {
+                ans.append(count);
+                ans.append(temp);
+                temp = s.charAt(i);
+                count = 0;
             }
             count++;
             i++;
         }
-        ans+=count.toString();
-        ans+=s.charAt(len-1);
-        return ans;
+        ans.append(count);
+        ans.append(temp);
+        return ans.toString();
     }
+
     public String countAndSay(int n) {
         String a = "1";
 
-        for(int i =0;i<n-1;i++)
-        {
+        for (int i = 0; i < n - 1; i++) {
             a = func1(a);
         }
         return a;
