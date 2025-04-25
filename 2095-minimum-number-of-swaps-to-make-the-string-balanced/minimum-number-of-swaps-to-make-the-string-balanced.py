4c93a3ea6ee99649643ef150b05d1c536148b00f
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-04-26 02:03:13 +0530

    Time: 227 ms (36.71%) | Memory: 23.4 MB (31.44%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,29 +1,14 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
-        stack = []
+        max_unbalanced = 0
+        balance = 0
+
         for char in s:
-            if(char == '['):
-                stack.append('[')
+            if char == '[':
+                balance += 1
             else:
-                if(len(stack)==0 or stack[-1]!='[' ):
-                    stack.append(']')
-                else:
-                    stack.pop()
-        ans=0
-        a=stack
-        print(a)
-        if(len(a)%2!=0):
-            ans=-1
-        elif(len(a)==0):
-            return 0
-        else:
-            i=0
-            j=len(a)-1
-            ans=0
-            while(i<j):
-                if(a[i]=="]" and a[j]=="["):
-                    ans+=1
-                i+=1
-                j-=1
-        return (ans+1)//2
-        
+                balance -= 1
+                if balance < 0:
+                    max_unbalanced = max(max_unbalanced, -balance)
+
+        return (max_unbalanced + 1) // 2
