2626999d0db98244affd77c40ef4ce6ff34d2f99
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-04-29 15:37:33 +0530

    Time: 163 ms (5.10%) | Memory: 45.5 MB (31.39%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+        int len = s.length();
+        int gap = (numRows*2) - 2;
+        int tempgap= numRows - 2;
+        int ind2 = 0;
+        String ans = "";
+        for(int i = 0;i<numRows;i++)
+        {
+            int ind = i;
+            while(ind-i-ind2<len)
+            {
+                System.out.print(gap);
+                if(ind2!=0 && ind2<tempgap+1)
+                {
+                    if(ind-i-ind2>0 && ind-i-ind2<len)
+                    {
+                        ans+=s.charAt(ind-i-ind2);
+                    }
+                }
+                if(ind<len) ans+=s.charAt(ind);
+                ind+=gap;
+            }
+            ind2++;
+        }
+        return ans;
+        
+    }
+}
