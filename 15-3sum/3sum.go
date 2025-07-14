af9efb51d50640b12e87b2d50e1eabd53e607db7
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-07-14 16:25:54 +0530

    Time: 133 ms (18.89%) | Memory: 10.1 MB (22.16%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -1,32 +1,28 @@
-import "sort"
-
 func threeSum(nums []int) [][]int {
+	size := len(nums)
+	mp := make(map[[3]int]bool)
 	sort.Ints(nums)
-	var combos [][]int
-	amt := len(nums)
+	ans := make([][]int,0)
 
-	for i := 0; i < amt-2; i++ {
-		if i > 0 && nums[i] == nums[i-1] {
-			continue
-		}
-		j, k := i+1, amt-1
-		for k > j {
-			total := nums[i] + nums[j] + nums[k]
-
-			if total > 0 {
-				k--
-			} else if total < 0 {
-				j++
-			} else {
-				combos = append(combos, []int{nums[i], nums[j], nums[k]})
-				j++
-
-				for nums[j] == nums[j-1] && k > j {
-					j++
-				}
-			}
-		}
+	for i := 0; i < size-2; i++ {
+		j := i + 1
+		k := size - 1
+        for j<k{
+            sum:=(nums[i] + nums[j] + nums[k])
+            
+		if sum == 0 {
+			mp[[3]int{nums[i] ,nums[j] ,nums[k]}] = true
+            j++
+            k--
+		}else if sum>0{
+            k--
+        }else{
+            j++
+        }
+        }
 	}
-
-	return combos
+	for k,_ := range mp{
+	    ans = append(ans,k[0:])
+	}
+	return ans
 }
