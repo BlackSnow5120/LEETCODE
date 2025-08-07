1af3c0cab8eb914f60043b85b710923691ae3ad2
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-08-07 16:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (22.08%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Rotate the matrix 90 degrees clockwise in-place.
+        """
+        if not matrix or not matrix[0]:
+            return
+        
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for row in matrix:
+            row.reverse()
