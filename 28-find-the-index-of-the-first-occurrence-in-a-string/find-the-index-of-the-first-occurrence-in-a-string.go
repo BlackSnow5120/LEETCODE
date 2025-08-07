a62c1e0a0768dbf8924380097cbf6b82c590f8ad
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-08-07 16:22:49 +0530

    Time: 7 ms (1.97%) | Memory: 4 MB (49.31%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,20 @@
+func strStr(haystack string, needle string) int {
+	j := 0
+	i := 0
+	for i = 0; i < len(haystack) && j < len(needle); i++ {
+		if haystack[i] != needle[j] && j!=0{
+            i = i-j+1
+			j = 0
+		}
+		if haystack[i] == needle[j] {
+			j++   
+		}
+        fmt.Print(i)
+	}
+	if j == len(needle) {
+		return i - j
+	} else {
+		return -1
+	}
+
+}
