e1ab8287da25f106c49ee893dc55bef9377f95ad
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-08-07 16:49:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.97%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -0,0 +1,31 @@
+func setZeroes(matrix [][]int) {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return
+	}
+	n := len(matrix)
+	m := len(matrix[0])
+	arr := []int{}
+	arr1 := []int{}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if matrix[i][j] == 0 {
+                arr = append(arr,i)
+                arr1 = append(arr1,j)
+			}
+		}
+	}
+	for _, val := range arr1 {
+		for k := 0; k < n; k++ {
+			matrix[k][val] = 0
+		}
+
+	}
+	for _, val := range arr {
+
+    		for k := 0; k < m; k++ {
+			matrix[val][k] = 0
+		}
+	}
+
+}
