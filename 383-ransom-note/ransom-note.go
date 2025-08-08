f11dedf189760a51994e25dbc617b202d6287f44
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2025-08-08 12:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 5.6 MB (99.26%) - LeetSync

--- 383-ransom-note/ransom-note.go
+++ 383-ransom-note/ransom-note.go
@@ -1,13 +1,17 @@
 func canConstruct(ransomNote string, magazine string) bool {
-	mp := make(map[byte]int)
+	count := [26]int{}
+
 	for i := 0; i < len(magazine); i++ {
-		mp[magazine[i]]++
+		count[magazine[i]-'a']++
 	}
+
 	for i := 0; i < len(ransomNote); i++ {
-		mp[ransomNote[i]]--
-		if mp[ransomNote[i]] == -1 {
+		idx := ransomNote[i] - 'a'
+		count[idx]--
+		if count[idx] < 0 {
 			return false
 		}
 	}
+
 	return true
 }
