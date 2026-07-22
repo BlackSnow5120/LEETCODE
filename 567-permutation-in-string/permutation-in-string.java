ceadc7271b94be444ba1e31c9822b4316c8a659e
Bhuvain Jhamb <124060979+BlackSnow5120@users.noreply.github.com>
2026-07-22 22:25:04 +0530

    Time: 124 ms (12.21%) | Memory: 47.6 MB (5.83%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int left = 0;
+        int right = 0;
+        HashMap<Character, Integer> mp1 = new HashMap<>();
+        HashMap<Character, Integer> mp2 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            mp1.put(s1.charAt(i), mp1.getOrDefault(s1.charAt(i), 0) + 1);
+        }
+        while (right < s2.length()) {
+            char rightChar = s2.charAt(right);
+            mp2.put(rightChar, mp2.getOrDefault(rightChar, 0) + 1);
+            while (left <= right && mp2.get(s2.charAt(right)) > mp1.getOrDefault(s2.charAt(right), 0)) {
+                System.out.println(s2.charAt(left));
+                mp2.put(s2.charAt(left), mp2.get(s2.charAt(left)) - 1);
+                left++;
+            }
+            if (right-left + 1 == s1.length()) {
+                return true;
+            }
+            right++;
+        }
+        return false;
+
+    }
+}
